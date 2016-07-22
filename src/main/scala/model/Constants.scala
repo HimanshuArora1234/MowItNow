db3b4ffe92cd941abf23b3495a7a89853f328c90
himanshuarora <himanshu.arora.alw@gmail.com>
2016-07-22 23:10:46 +0200

    Data modeling of MowItNow problem

---
+++ src/main/scala/model/Constants.scala
@@ -0,0 +1,9 @@
+package model
+
+object Direction extends Enumeration {
+  val N, E, W, S = Value
+}
+
+object Commande extends Enumeration {
+  val D, G, A = Value
+}
---
+++ src/main/scala/model/Tondeuse.scala
@@ -0,0 +1,6 @@
+package model
+
+/**
+ * Created by arorah on 22/07/2016.
+ */
+case class Tondeuse (coordonnees: Coordonnees, direction: Direction.Value)
