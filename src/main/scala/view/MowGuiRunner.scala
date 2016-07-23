e5b05b7db948f855b43337a4896cc4ed343893c1
himanshuarora <himanshu.arora.alw@gmail.com>
2016-07-23 23:51:46 +0200

    GUI runner, calling parsing & tondeuse services to display output dialog boxes

--- src/main/scala/view/MowGuiRunner.scala
+++ src/main/scala/view/MowGuiRunner.scala
@@ -1,16 +1,21 @@
 package view
 
 
+import javax.smartcardio.CommandAPDU
+
+import model.{Tondeuse, Commande}
 
 import scalafx.Includes._
 import scalafx.application.JFXApp
 import scalafx.event.ActionEvent
 import scalafx.scene.Scene
-import scalafx.scene.control.Hyperlink
+import scalafx.scene.control.Alert.AlertType
+import scalafx.scene.control.{Alert, Hyperlink}
 import scalafx.scene.image.Image
 import scalafx.scene.layout.StackPane
 import scalafx.stage.FileChooser
 
+import service._
 /**
  * ScalaFX GUI object to run the MowItNow application.
  */
@@ -32,6 +37,32 @@ object MowGuiRunner extends JFXApp {
 	chooseLink.onAction = (e: ActionEvent) => {
 		val fileChooser = new FileChooser() {title = "Pick an input file"}
 		val inputFile = fileChooser.showOpenDialog(stage)
+		if(inputFile != null) {
+			val fileParseService = new ParsingService()
+			val tondeuseService = new TondeuseService()
+			val pelouse = fileParseService.getPelous(inputFile)
+			val tondeuses = fileParseService.getTondeuses(inputFile)
+			val commandes = fileParseService.getCommandes(inputFile)
+			pelouse match {
+				case Some(p) =>
+					tondeuses.zipWithIndex.map {
+					case (tondeuse, index) if(commandes.length >= index + 1) => {
+						val movedTondeuse = commandes(index).foldLeft(tondeuse)((accumulator, command)  => tondeuseService.moveTondeuse(accumulator, p, command))
+						new Alert(AlertType.Information) {
+							title = "Output"
+							headerText = "Position of tondeuse " + (index + 1)
+							contentText = movedTondeuse.print
+						}.showAndWait
+					}
+				}
+				case None => new Alert(AlertType.Error) {
+					title = "Error"
+					headerText = "No pelouse coodiantes found"
+					contentText = "Provide a valide input file"
+				}.showAndWait
+			}
+
+		}
 	}
 	rootPane.children.add(chooseLink)
 }
