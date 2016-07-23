4cfb6263caed32f136c2c7d0941f085abe3eb8c8
himanshuarora <himanshu.arora.alw@gmail.com>
2016-07-23 23:50:22 +0200

    Tondeuse class print function

--- src/main/scala/model/Tondeuse.scala
+++ src/main/scala/model/Tondeuse.scala
@@ -3,4 +3,7 @@ package model
 /**
  * Created by arorah on 22/07/2016.
  */
-case class Tondeuse (coordonnees: Coordonnees, direction: Direction.Value)
+case class Tondeuse (coordonnees: Coordonnees, direction: Direction.Value) {
+	def print = this.coordonnees.x + " " + this.coordonnees.y + " " + this.direction.toString
+}
+
